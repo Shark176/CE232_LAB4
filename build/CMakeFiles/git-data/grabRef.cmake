db94de87d4d7ed9299f702b5cc889cd87bd93190
Shark176 <21522760@gm.uit.edu.vn>
2024-05-28 15:03:08 +0700

    Update Demo

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/Phan_Thanh_Tuan/UIT-Online/HK6/TH_TKHTNKD/LAB4_MQTT_TCP/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "E:/DATA/Espressif/frameworks/esp-idf-v5.2/.git")
+set(GIT_DIR "D:/Phan_Thanh_Tuan/UIT-Online/HK6/TH_TKHTNKD/LAB4_MQTT_TCP/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
